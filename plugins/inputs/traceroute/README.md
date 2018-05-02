fd6ef1726b9ebc3133360e0ed79194940d5f48ff
MFUNG <fungyuensang_mf@hotmail.com>
2018-05-02 15:01:43 -0700

    Traceroute input plugin simple stubs
    test stub & compile modifications
    
    working exec call, output parsing stubs
    
    traceroute route parsing + accompanying tests
    
    header line parser + accompanying tests
    
    updated measurement documentation
    
    refractored some regexp into file scope to reduce redundancy
    
    credentials & acknowledgements
    
    simple accumulator output
    
    plugin now produces hop info data
    
    input parameters + docs
    
    sample queries & example output
    
    renamed hop_rtt to hop_rtt_ms so field name has units
    
    additional parameters: nqueries, no host name, icmp
    
    traceroute_hop_data fields are not unique per hop
    
    moved hop_number from tag to field
    
    isolated parser logic from telegraf interfacing logic
    
    isolated argument & parser logic

---
+++ plugins/inputs/traceroute/README.md
@@ -0,0 +1,104 @@
+# traceroute Input Plugin
+
+The traceroute plugin provides routing information given end host.
+
+### Configuration:
+
+```toml
+# NOTE: this plugin forks the traceroute command. You may need to set capabilities
+# via setcap cap_net_raw+p /bin/traceroute
+[[inputs.traceroute]]
+  ## List of urls to traceroute
+  urls = ["www.google.com"] # required
+  ## per-traceroute timeout, in s. 0 == no timeout
+  ## it is highly recommended to set this value to match the telegraf interval
+  # response_timeout = 0.0
+  ## wait time per probe in seconds (traceroute -w <WAITTIME>)
+  # waittime = 5.0
+  ## starting TTL of packet (traceroute -f <FIRST_TTL>)
+  # first_ttl = 1
+  ## maximum number of hops (hence TTL) traceroute will probe (traceroute -m <MAX_TTL>)
+  # max_ttl = 30
+  ## number of probe packets sent per hop (traceroute -q <NQUERIES>)
+  # nqueries = 3
+  ## do not try to map IP addresses to host names (traceroute -n)
+  # no_host_name = false
+  ## use ICMP packets (traceroute -I)
+  # icmp = false
+  ## source interface/address to traceroute from (traceroute -i <INTERFACE/SRC_ADDR>)
+  # interface = ""
+```
+
+### Metrics:
+
+- traceroute
+  - tags:
+    - target_fqdn 
+    - target_ip (IPv4 string)
+  - fields:
+    - result_code
+        - 0:success
+      	- 1:no such host
+    - number_of_hops (int, # of hops made)
+
+- traceroute_hop_data
+  - tags:
+    - target_fqdn
+    - target_ip (IPv4 string)
+    - column_number (zero-indexed value representing which column of the traceroute output the data resides in)
+    - hop_fqdn
+    - hop_ip (IPv4 string)
+    - hop_number (string)
+  - fields:
+    - hop_rtt_ms (round trip time in ms)
+
+### Sample Queries:
+
+Get traceroute information given host
+```
+SELECT *
+FROM "traceroute"
+WHERE "target_fqdn"='www.google.com'
+```
+
+Get average round trip team for each top given time
+```
+SELECT MEAN("hop_rtt_ms")
+FROM "traceroute_hop_data"
+WHERE "time"=1453831884664956455
+GROUP BY "hop_number"
+```
+
+### Example Output:
+
+#### traceroute
+```
+> traceroute,host=m1.cloudpbx.ca,target_fqdn=www.google.com,target_ip=172.217.0.100 number_of_hops=6i 1525474707000000000
+```
+
+#### traceroute_hop_data
+```
+> traceroute_hop_data,column_number=0,host=m1.cloudpbx.ca,target_fqdn=www.google.com,target_ip=172.217.0.100 hop_fqdn="165.227.32.253",hop_ip="165.227.32.253",hop_number=1,hop_rtt_ms=3.5250000953674316 1525474707000000000
+> traceroute_hop_data,column_number=1,host=m1.cloudpbx.ca,target_fqdn=www.google.com,target_ip=172.217.0.100 hop_fqdn="165.227.32.254",hop_ip="165.227.32.254",hop_number=1,hop_rtt_ms=3.071000099182129 1525474707000000000
+> traceroute_hop_data,column_number=2,host=m1.cloudpbx.ca,target_fqdn=www.google.com,target_ip=172.217.0.100 hop_fqdn="165.227.32.253",hop_ip="165.227.32.253",hop_number=1,hop_rtt_ms=3.4200000762939453 1525474707000000000
+> traceroute_hop_data,column_number=0,host=m1.cloudpbx.ca,target_fqdn=www.google.com,target_ip=172.217.0.100 hop_fqdn="138.197.249.78",hop_ip="138.197.249.78",hop_number=2,hop_rtt_ms=3.4010000228881836 1525474707000000000
+> traceroute_hop_data,column_number=1,host=m1.cloudpbx.ca,target_fqdn=www.google.com,target_ip=172.217.0.100 hop_fqdn="138.197.249.82",hop_ip="138.197.249.82",hop_number=2,hop_rtt_ms=3.5429999828338623 1525474707000000000
+> traceroute_hop_data,column_number=2,host=m1.cloudpbx.ca,target_fqdn=www.google.com,target_ip=172.217.0.100 hop_fqdn="138.197.249.78",hop_ip="138.197.249.78",hop_number=2,hop_rtt_ms=3.3429999351501465 1525474707000000000
+> traceroute_hop_data,column_number=0,host=m1.cloudpbx.ca,target_fqdn=www.google.com,target_ip=172.217.0.100 hop_fqdn="72.14.219.10",hop_ip="72.14.219.10",hop_number=3,hop_rtt_ms=2.0139999389648438 1525474707000000000
+> traceroute_hop_data,column_number=1,host=m1.cloudpbx.ca,target_fqdn=www.google.com,target_ip=172.217.0.100 hop_fqdn="162.243.190.33",hop_ip="162.243.190.33",hop_number=3,hop_rtt_ms=3.315999984741211 1525474707000000000
+> traceroute_hop_data,column_number=2,host=m1.cloudpbx.ca,target_fqdn=www.google.com,target_ip=172.217.0.100 hop_fqdn="162.243.190.33",hop_ip="162.243.190.33",hop_number=3,hop_rtt_ms=2.9059998989105225 1525474707000000000
+> traceroute_hop_data,column_number=0,host=m1.cloudpbx.ca,target_fqdn=www.google.com,target_ip=172.217.0.100 hop_fqdn="108.170.250.225",hop_ip="108.170.250.225",hop_number=4,hop_rtt_ms=1.559000015258789 1525474707000000000
+> traceroute_hop_data,column_number=1,host=m1.cloudpbx.ca,target_fqdn=www.google.com,target_ip=172.217.0.100 hop_fqdn="108.170.250.241",hop_ip="108.170.250.241",hop_number=4,hop_rtt_ms=0.7829999923706055 1525474707000000000
+> traceroute_hop_data,column_number=2,host=m1.cloudpbx.ca,target_fqdn=www.google.com,target_ip=172.217.0.100 hop_fqdn="108.170.250.225",hop_ip="108.170.250.225",hop_number=4,hop_rtt_ms=1.5080000162124634 1525474707000000000
+> traceroute_hop_data,column_number=0,host=m1.cloudpbx.ca,target_fqdn=www.google.com,target_ip=172.217.0.100 hop_fqdn="216.239.35.229",hop_ip="216.239.35.229",hop_number=5,hop_rtt_ms=2.947000026702881 1525474707000000000
+> traceroute_hop_data,column_number=1,host=m1.cloudpbx.ca,target_fqdn=www.google.com,target_ip=172.217.0.100 hop_fqdn="216.239.35.231",hop_ip="216.239.35.231",hop_number=5,hop_rtt_ms=2.9040000438690186 1525474707000000000
+> traceroute_hop_data,column_number=2,host=m1.cloudpbx.ca,target_fqdn=www.google.com,target_ip=172.217.0.100 hop_fqdn="216.239.35.229",hop_ip="216.239.35.229",hop_number=5,hop_rtt_ms=2.5940001010894775 1525474707000000000
+> traceroute_hop_data,column_number=0,host=m1.cloudpbx.ca,target_fqdn=www.google.com,target_ip=172.217.0.100 hop_fqdn="yyz10s13-in-f4.1e100.net",hop_ip="172.217.0.100",hop_number=6,hop_rtt_ms=2.010999917984009 1525474707000000000
+> traceroute_hop_data,column_number=1,host=m1.cloudpbx.ca,target_fqdn=www.google.com,target_ip=172.217.0.100 hop_fqdn="yyz10s13-in-f4.1e100.net",hop_ip="172.217.0.100",hop_number=6,hop_rtt_ms=0.6510000228881836 1525474707000000000
+> traceroute_hop_data,column_number=2,host=m1.cloudpbx.ca,target_fqdn=www.google.com,target_ip=172.217.0.100 hop_fqdn="yyz10s13-in-f4.1e100.net",hop_ip="172.217.0.100",hop_number=6,hop_rtt_ms=0.6190000176429749 1525474707000000000
+```
+
+
+[mattfung](https://github.com/mattfung)
+
+Sponsored by [CloudPBX](http://CloudPBX.ca) with generous support by the NSERC Experience Award.
---
+++ plugins/inputs/traceroute/traceroute_test.go
@@ -0,0 +1,236 @@
+package traceroute
+
+import (
+	//"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	LinuxTracerouteOutput = `
+traceroute to google.com (172.217.0.238), 30 hops max, 60 byte packets
+ 1  165.227.32.254 (165.227.32.254)  1.206 ms 165.227.32.253 (165.227.32.253)  1.188 ms 165.227.32.254 (165.227.32.254)  1.143 msg
+ 2  138.197.249.78 (138.197.249.78)  0.985 ms 138.197.249.86 (138.197.249.86)  0.939 ms 138.197.249.90 (138.197.249.90)  1.181 ms
+ 3  72.14.219.10 (72.14.219.10)  0.818 ms 162.243.190.33 (162.243.190.33)  0.952 ms  0.941 ms
+ 4  108.170.250.225 (108.170.250.225)  0.825 ms  0.970 ms 108.170.250.241 (108.170.250.241)  1.007 ms
+ 5  108.170.226.217 (108.170.226.217)  0.995 ms 108.170.226.219 (108.170.226.219)  1.033 ms 108.170.226.217 (108.170.226.217)  1.003 ms
+ 6  dfw06s38-in-f14.1e100.net (172.217.0.238)  1.187 ms  0.722 ms  0.545 ms
+`
+	LinuxTracerouteHeader = `traceroute to google.com (172.217.0.238), 30 hops max, 60 byte packets`
+	LinuxTracerouteFqdn   = `google.com`
+	LinuxTracerouteIp     = `172.217.0.238`
+)
+
+func MockHostTracerouter(timeout float64, args ...string) (string, error) {
+	return LinuxTracerouteOutput, nil
+}
+
+func TestConstructorSimple(t *testing.T) {
+	var tr *Traceroute
+	tr = &Traceroute{
+		tracerouteMethod: MockHostTracerouter,
+	}
+	sampleConfig := tr.SampleConfig()
+	//fmt.Println(sampleConfig)
+	assert.NotNil(t, sampleConfig)
+}
+
+/*
+func TestHostTracerouterSimple(t *testing.T) {
+	var tr *Traceroute
+	tr = &Traceroute{}
+	args := tr.args("google.com")
+	_, err := hostTracerouter(0, args...)
+	if err != nil {
+		t.Fatal("call failed:", err)
+	}
+}
+*/
+
+func TestHostTracerouteBadHost(t *testing.T) {
+	var tr *Traceroute
+	tr = &Traceroute{}
+	args := tr.args("badhost")
+	_, err := hostTracerouter(3, args...)
+	assert.Error(t, err)
+}
+
+func TestFindNumberOfHops(t *testing.T) {
+	numHops := findNumberOfHops(LinuxTracerouteOutput)
+	assert.Equal(t, 6, numHops, "6 hops made by packet")
+}
+
+var SampleTracerouteLine = `12  54.239.110.174 (54.239.110.174)  22.609 ms 54.239.110.130 (54.239.110.130)  26.629 ms 54.239.110.183 (54.239.110.183)  34.258 ms`
+
+func TestGetHopNumber(t *testing.T) {
+	hopNum, err := findHopNumber(SampleTracerouteLine)
+	assert.NoError(t, err)
+	assert.Equal(t, 12, hopNum, "Traceroute line is the 12th hop")
+}
+
+var (
+	NormalTracerouteLine      = ` 6  yyz10s03-in-f3.1e100.net (172.217.0.227)  1.480 ms  1.244 ms  0.417 ms`
+	NormalTracerouteEntries   = []string{"yyz10s03-in-f3.1e100.net (172.217.0.227)  1.480 ms", "1.244 ms", "0.417 ms"}
+	NormalTracerouteHopNumber = 6
+	NormalTracerouteHopInfo   = []TracerouteHopInfo{
+		TracerouteHopInfo{
+			HopNumber: NormalTracerouteHopNumber,
+			ColumnNum: 0,
+			Fqdn:      "yyz10s03-in-f3.1e100.net",
+			Ip:        "172.217.0.227",
+			RTT:       1.480,
+		},
+		TracerouteHopInfo{
+			HopNumber: NormalTracerouteHopNumber,
+			ColumnNum: 1,
+			Fqdn:      "yyz10s03-in-f3.1e100.net",
+			Ip:        "172.217.0.227",
+			RTT:       1.244,
+		},
+		TracerouteHopInfo{
+			HopNumber: NormalTracerouteHopNumber,
+			ColumnNum: 2,
+			Fqdn:      "yyz10s03-in-f3.1e100.net",
+			Ip:        "172.217.0.227",
+			RTT:       0.417,
+		},
+	}
+)
+var (
+	SomeVoidTracerouteLine      = `14  54.239.110.152 (54.239.110.152)  27.198 ms * 54.239.110.247 (54.239.110.247)  37.625 ms`
+	SomeVoidTracerouteEntries   = []string{"54.239.110.152 (54.239.110.152)  27.198 ms", "*", "54.239.110.247 (54.239.110.247)  37.625 ms"}
+	SomeVoidTracerouteHopNumber = 14
+	SomeVoidTracerouteHopInfo   = []TracerouteHopInfo{
+		TracerouteHopInfo{
+			HopNumber: SomeVoidTracerouteHopNumber,
+			ColumnNum: 0,
+			Fqdn:      "54.239.110.152",
+			Ip:        "54.239.110.152",
+			RTT:       27.198,
+		},
+		TracerouteHopInfo{
+			HopNumber: SomeVoidTracerouteHopNumber,
+			ColumnNum: 2,
+			Fqdn:      "54.239.110.247",
+			Ip:        "54.239.110.247",
+			RTT:       37.625,
+		},
+	}
+)
+
+var (
+	NoHostTracerouteLine      = `10  129.250.2.81  186.767 ms`
+	NoHostTracerouteEntries   = []string{"129.250.2.81  186.767 ms"}
+	NoHostTracerouteHopNumber = 10
+	NoHostTracerouteHopInfo   = []TracerouteHopInfo{
+		TracerouteHopInfo{
+			HopNumber: NoHostTracerouteHopNumber,
+			ColumnNum: 0,
+			Fqdn:      "129.250.2.81",
+			Ip:        "129.250.2.81",
+			RTT:       186.767,
+		},
+	}
+)
+
+var (
+	AllVoidTracerouteLine      = `5  * * *`
+	AllVoidTracerouteEntries   = []string{"*", "*", "*"}
+	AllVoidTracerouteHopNumber = 5
+)
+
+func TestFindColumnEntries(t *testing.T) {
+	var entries []string
+	entries = findColumnEntries(NormalTracerouteLine)
+	assert.Equal(t, 3, len(entries), "3 entries")
+	assert.True(t, reflect.DeepEqual(NormalTracerouteEntries, entries), "Expected: %s, Actual: %s", entries, NormalTracerouteEntries)
+
+	entries = findColumnEntries(SomeVoidTracerouteLine)
+	assert.Equal(t, 3, len(entries), "3 entries")
+	assert.True(t, reflect.DeepEqual(SomeVoidTracerouteEntries, entries), "Expected: %s, Actual: %s", entries, SomeVoidTracerouteEntries)
+
+	entries = findColumnEntries(NoHostTracerouteLine)
+	assert.Equal(t, 1, len(entries), "1 entry")
+	assert.True(t, reflect.DeepEqual(NoHostTracerouteEntries, entries), "Expected: %s, Actual: %s", entries, NoHostTracerouteEntries)
+
+	entries = findColumnEntries(AllVoidTracerouteLine)
+	assert.Equal(t, 3, len(entries), "3 entries")
+	assert.True(t, reflect.DeepEqual(AllVoidTracerouteEntries, entries), "Expected: %s, Actual: %s", entries, AllVoidTracerouteEntries)
+}
+
+var (
+	IpFqdnColumnEntry    = `12  54.239.110.174 (54.239.110.174)  22.609 ms`
+	HttpFqdnColumnEntry  = `yyz10s03-in-f3.1e100.net (172.217.0.227)  1.480 ms`
+	CarryOverColumnEntry = `0.417 ms`
+	NoHostColumnEntry    = `3  192.168.1.1  2.854 ms`
+)
+
+func TestProcessTracerouteColumnEntry(t *testing.T) {
+	var fqdn, ip string
+	var rtt float32
+	var err error
+	acceptableDelta := 0.0005
+
+	fqdn, ip, rtt, err = processTracerouteColumnEntry(IpFqdnColumnEntry, 0, "", "")
+	assert.NoError(t, err)
+	assert.Equal(t, "54.239.110.174", fqdn, "fqdn")
+	assert.Equal(t, "54.239.110.174", ip, "ip")
+	assert.InDelta(t, 22.609, rtt, acceptableDelta, "rtt")
+
+	fqdn, ip, rtt, err = processTracerouteColumnEntry(HttpFqdnColumnEntry, 3, "something.not.useful.org", "255.255.255.255")
+	assert.NoError(t, err)
+	assert.Equal(t, "yyz10s03-in-f3.1e100.net", fqdn, "fqdn")
+	assert.Equal(t, "172.217.0.227", ip, "ip")
+	assert.InDelta(t, 1.480, rtt, acceptableDelta, "rtt")
+
+	carryOverFqdn := "wildmadagascar.org"
+	carryOverIp := "75.101.140.9"
+	fqdn, ip, rtt, err = processTracerouteColumnEntry(CarryOverColumnEntry, 1, carryOverFqdn, carryOverIp)
+	assert.NoError(t, err)
+	assert.Equal(t, carryOverFqdn, fqdn, "fqdn")
+	assert.Equal(t, carryOverIp, ip, "ip")
+	assert.InDelta(t, 0.417, rtt, acceptableDelta, "rtt")
+
+	fqdn, ip, rtt, err = processTracerouteColumnEntry(NoHostColumnEntry, 0, "", "")
+	assert.NoError(t, err)
+	assert.Equal(t, "192.168.1.1", fqdn, "fqdn")
+	assert.Equal(t, "", ip, "ip")
+	assert.InDelta(t, 2.854, rtt, acceptableDelta, "rtt")
+
+}
+
+func TestProcessTracerouteHopLine(t *testing.T) {
+	var (
+		//hopNumber int
+		hopInfo []TracerouteHopInfo
+		err     error
+	)
+
+	hopInfo, err = processTracerouteHopLine(NormalTracerouteLine)
+	assert.NoError(t, err)
+	//assert.Equal(t, NormalTracerouteHopNumber, hopNumber, "hopNumber")
+	assert.True(t, reflect.DeepEqual(NormalTracerouteHopInfo, hopInfo), "Expected: %s Actual: %s", NormalTracerouteHopInfo, hopInfo)
+
+	hopInfo, err = processTracerouteHopLine(SomeVoidTracerouteLine)
+	assert.NoError(t, err)
+	//assert.Equal(t, SomeVoidTracerouteHopNumber, hopNumber, "hopNumber")
+	assert.True(t, reflect.DeepEqual(SomeVoidTracerouteHopInfo, hopInfo), "Expected: %s Actual: %s", SomeVoidTracerouteHopInfo, hopInfo)
+
+	hopInfo, err = processTracerouteHopLine(NoHostTracerouteLine)
+	assert.NoError(t, err)
+	//assert.Equal(t, NoHostTracerouteHopNumber, hopNumber, "hopNumber")
+	assert.True(t, reflect.DeepEqual(NoHostTracerouteHopInfo, hopInfo), "Expected: %s Actual: %s", NoHostTracerouteHopInfo, hopInfo)
+
+	hopInfo, err = processTracerouteHopLine(AllVoidTracerouteLine)
+	assert.NoError(t, err)
+	//assert.Equal(t, AllVoidTracerouteHopNumber, hopNumber, "hopNumber")
+	assert.True(t, reflect.DeepEqual([]TracerouteHopInfo{}, hopInfo), "Expected: %s Actual: %s", []TracerouteHopInfo{}, hopInfo)
+}
+
+func TestProcessTracerouteHeaderLine(t *testing.T) {
+	fqdn, ip := processTracerouteHeaderLine(LinuxTracerouteHeader)
+	assert.Equal(t, LinuxTracerouteFqdn, fqdn, "fqdn")
+	assert.Equal(t, LinuxTracerouteIp, ip, "ip")
+}
